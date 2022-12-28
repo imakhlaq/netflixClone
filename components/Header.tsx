07e8cabf099e19f6b136b7314de1fc4832711d50
imakhlaq <imakhlaqXD@gmail.com>
2022-12-29 00:54:37 +0530

    header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,39 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BellIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header>
-      <div>Header</div>
-      <div></div>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="logo"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+      <div className="flex items-center space-x-4 text-sm font-light ">
+        <MagnifyingGlassIcon className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className=" h-6 w-6" />
+        <Link href="/accounts">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="acc"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   );
 };
